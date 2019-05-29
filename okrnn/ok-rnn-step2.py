7a9a62a4cc49a11c72b3d0008260c36ecd5b4066
John Alamina <john.alamina@gmail.com>
2019-05-29 09:43:42 +0100

    rm okrnn

--- okrnn/ok-rnn-step2.py
+++
@@ -1,3 +0,0 @@
-# display some stats on the data
-epoch_size = len(codetext) // (BATCHSIZE * SEQLEN)
-txt.print_data_stats(len(codetext), len(valitext), epoch_size)
--- okrnn/ok-rnn-step4.py
+++
@@ -1,9 +0,0 @@
-# using a NLAYERS=3 layers of GRU cells, unrolled SEQLEN=30 times
-# dynamic_rnn infers SEQLEN from the size of the inputs Xo
-
-# How to properly apply dropout in RNNs: see README.md
-cells = [rnn.GRUCell(INTERNALSIZE) for _ in range(NLAYERS)]
-# "naive dropout" implementation
-dropcells = [rnn.DropoutWrapper(cell,input_keep_prob=pkeep) for cell in cells]
-multicell = rnn.MultiRNNCell(dropcells, state_is_tuple=False)
-multicell = rnn.DropoutWrapper(multicell, output_keep_prob=pkeep)  # dropout for the softmax layer
