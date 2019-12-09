d2b7fdffe2f6177fc3dc477e1e5b48b5abe5e53c
John Alamina <john.alamina@netsells.co.uk>
2019-12-09 04:47:05 +0000

    scatter impl00

--- gallery_1d_jupyter/espmine/feature_scatter.py
+++ gallery_1d_jupyter/espmine/feature_scatter.py
@@ -25,4 +25,28 @@ class ScatterTransform(torch.nn.Module):
         # Zero padding
         logmel_feat = logmel_feat.masked_fill(
             make_pad_mask(ilens, logmel_feat, 1), 0.0)
+
+
+
+        # We now create the Scattering1D object that will be used to calculate the scattering coefficients.
+        # scattering = Scattering1D(J, T, Q)
+
+        # If we are using CUDA, the scattering transform object must be transferred to the GPU by calling its cuda() method. The data is similarly transferred.
+        # if use_cuda:
+        #     scattering.cuda()
+        #     x_all = x_all.cuda()
+        #     y_all = y_all.cuda()
+
+        # Compute the scattering transform for all signals in the dataset.
+        # Sx_all = scattering.forward(x_all)
+
+        # Since it does not carry useful information, we remove the zeroth-order scattering coefficients, which are always placed in the first channel of the scattering Tensor.
+        # Sx_all = Sx_all[:,1:,:]
+
+        # To increase discriminability, we take the logarithm of the scattering coefficients (after adding a small constant to make sure nothing blows up when scattering coefficients are close to zero).
+        # Sx_all = torch.log(torch.abs(Sx_all) + log_eps)
+
+        # Finally, we average along the last dimension (time) to get a time-shift invariant representation.
+        # Sx_all = torch.mean(Sx_all, dim=-1)
+
         return logmel_feat, ilens
