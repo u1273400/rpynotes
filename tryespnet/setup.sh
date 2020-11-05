9231d3f633db62276bfdee7201357561281a653b
Iyalla Alamina (Researcher) <John.Alamina@hud.ac.uk>
2020-11-05 13:42:06 +0000

    pytorch00

--- tryespnet/setup.sh
+++
@@ -1,6 +0,0 @@
-CUDAROOT='n:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v9.0'
-export KALDI_HOME=/home/u1273400/kaldi
-export PATH=$CUDAROOT/bin:$PATH
-export LD_LIBRARY_PATH=$CUDAROOT/lib64:$LD_LIBRARY_PATH
-export CUDA_HOME=$CUDAROOT
-export CUDA_PATH=$CUDAROOT
