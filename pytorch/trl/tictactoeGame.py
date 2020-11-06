6820b8a887347d603fd0c561e09e443823c3449d
John Alamina <john.alamina@gmail.com>
2020-11-06 21:14:44 +0000

    tg02

--- pytorch/trl/tictactoeGame.py
+++ pytorch/trl/tictactoeGame.py
@@ -32,98 +32,50 @@ class TicTacGame():
 
     def reset(self):
         self.gameState = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        self.XTurnToPlay = true;
-        self.winner = "TicTacToe Demo";
-        self.windex = -1;
+        self.XTurnToPlay = True
+        self.winner = "TicTacToe Demo"
+        self.windex = -1
 
-    def gamePlay():
-        if (self.isWinState() or se.isBoardFilled())
-        this.reset();
-        this.getNextState();
-        }
-        public
-        void
-        getNextState()
-        {
-            int
-        v = _next(0, 9);
-        while (this.gameState[v] != "-") {
-        v = _next(0, 9);
-        }
-        this.gameState[v] = this.XTurnToPlay ? "X": "O";
-        this.XTurnToPlay = !this.XTurnToPlay;
-        this.winner = this.isWinState()?(this.XTurnToPlay?"O":"X") + " wins": (this.isBoardFilled()
-                                                                               ?"game was a draw":this.winner);
-        // print('this.windex=${this.windex}');
-        // this.testWinState();
+    def gamePlay(self):
+        if (self.isWinState() or self.isBoardFilled())
+        self.reset()
+        self.getNextState()
 
-    }
+    def getNextState(self):
+        v = random.ranint(0, 9)
+        while (self.gameState[v] != "-"):
+            v = random.randint(0, 9)
+        self.gameState[v] =  "X" if self.XTurnToPlay else "O"
+        self.XTurnToPlay = not self.XTurnToPlay
+        self.winner = (("O" if self.XTurnToPlay else "X") + " wins") if self.isWinState() else ("game was a draw" if self.isBoardFilled() else self.winner)
+        # print('this.windex=${this.windex}');
+        # this.testWinState();
 
-    public
-    bool
-    isWinState()
-    {
-    var
-    winstate = false;
-    for (var i=0;i < this.winstates.Length;i++){
-    if (gameState[winstates[i][0]] != "-" & & gameState[winstates[i][0]] == gameState[winstates[i][1]] & & gameState[
-        winstates[i][1]] == gameState[winstates[i][2]]){
-    this.windex=i;
-    winstate=true;
-    break;
-    }
-    }
-    return winstate;
+    def isWinState(self):
+        winstate = False
+        for ( i in  range(len(self.winstates))):
+            if (self.gameState[self.winstates[i][0]] != "-" and self.gameState[self.winstates[i][0]]
+                    == self.gameState[self.winstates[i][1]] and
+                    self.gameState[self.winstates[i][1]] == self.gameState[self.winstates[i][2]]):
+                self.windex=i
+                winstate=True
+                break
+        return winstate
 
-}
+        def isBoardFilled(self):
+            return !this.gameState.ToList().Contains("-");
 
-public
-bool
-isBoardFilled()
-{
-return !this.gameState.ToList().Contains("-");
-}
+    def printState():
+        sb = new
+        StringBuilder();
+        for (var i=0;i < 3;i++){
+        for (var j=0;j < 3;j++)
+        sb.Append(this.gameState[i * 3+j]);
+        Console.WriteLine(sb.ToString());
+        sb.Clear();
 
-public
-void
-printState()
-{
-var
-sb = new
-StringBuilder();
-for (var i=0;i < 3;i++){
-for (var j=0;j < 3;j++)
-sb.Append(this.gameState[i * 3+j]);
-Console.WriteLine(sb.ToString());
-sb.Clear();
-}
-}
-
-/ **
-*Generates
-a
-positive
-random
-integer
-uniformly
-distributed
-on
-the
-range
-*
-from
-
-[min], inclusive, to[max], exclusive.
-* /
-private
-int
-_next(int
-min, int
-max) = > min + _random.Next(max - min);
-
-public
-void
-testWinState()
+        '''
+    def testWinState(self)
 {
 for (var i=0;i < this.winstates.Length;i++){
                                            // Console.WriteLine( @ "${this.winstates[i][0]}=${this.gameState[this.winstates[i][0]]}:${this.gameState[this.winstates[i][0]]!=" - "}");
@@ -132,13 +84,4 @@ for (var i=0;i < this.winstates.Length;i++){
 }
 }
 
-def forward(self, x):
-        x = self.dl1(x)
-        x = torch.relu(x)
-
-        x = self.dl2(x)
-        x = torch.relu(x)
-
-        x = self.output_layer(x)
-        x = torch.sigmoid(x)
-        return x
+'''
