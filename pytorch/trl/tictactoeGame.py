75f1c8112c253c930a42777ba9bcf2ed95610574
John Alamina <john.alamina@gmail.com>
2020-11-07 06:47:08 +0000

    tg03

--- pytorch/trl/tictactoeGame.py
+++ pytorch/trl/tictactoeGame.py
@@ -20,15 +20,15 @@ class TicTacGame():
     def gameLoop(self):
         tictactoe = TicTacGame()
         tictactoe.getNextState()
-        while (not tictactoe.isBoardFilled()):
+        while not tictactoe.isBoardFilled():
             tictactoe.getNextState()
-            if (tictactoe.isWinState()):
+            if tictactoe.isWinState():
                 break
-        if (tictactoe.isWinState()):
+        if tictactoe.isWinState():
             print(("O" if tictactoe.XTurnToPlay else "X") , "wins");
-        else
+        else:
             print("game was a draw")
-            tictactoe.printState()
+        tictactoe.printState()
 
     def reset(self):
         self.gameState = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
@@ -37,15 +37,15 @@ class TicTacGame():
         self.windex = -1
 
     def gamePlay(self):
-        if (self.isWinState() or self.isBoardFilled())
-        self.reset()
+        if self.isWinState() or self.isBoardFilled():
+            self.reset()
         self.getNextState()
 
     def getNextState(self):
-        v = random.ranint(0, 9)
-        while (self.gameState[v] != "-"):
-            v = random.randint(0, 9)
-        self.gameState[v] =  "X" if self.XTurnToPlay else "O"
+        v = random.randint(0, 8)
+        while self.gameState[v] != "-":
+            v = random.randint(0, 8)
+        self.gameState[v] = "X" if self.XTurnToPlay else "O"
         self.XTurnToPlay = not self.XTurnToPlay
         self.winner = (("O" if self.XTurnToPlay else "X") + " wins") if self.isWinState() else ("game was a draw" if self.isBoardFilled() else self.winner)
         # print('this.windex=${this.windex}');
@@ -53,28 +53,36 @@ class TicTacGame():
 
     def isWinState(self):
         winstate = False
-        for ( i in  range(len(self.winstates))):
+        for i in range(len(self.winstates)):
             if (self.gameState[self.winstates[i][0]] != "-" and self.gameState[self.winstates[i][0]]
                     == self.gameState[self.winstates[i][1]] and
                     self.gameState[self.winstates[i][1]] == self.gameState[self.winstates[i][2]]):
                 self.windex=i
-                winstate=True
+                winstate = True
                 break
         return winstate
 
-        def isBoardFilled(self):
-            return !this.gameState.ToList().Contains("-");
+    def isBoardFilled(self):
+        return "-" not in self.gameState
 
-    def printState():
-        sb = new
-        StringBuilder();
-        for (var i=0;i < 3;i++){
-        for (var j=0;j < 3;j++)
-        sb.Append(this.gameState[i * 3+j]);
-        Console.WriteLine(sb.ToString());
-        sb.Clear();
+    def printState(self):
+        sb = ""
+        for i in range(3):
+            for j in range(3):
+                sb += self.gameState[i * 3+j]
+            print(sb)
+            sb=""
 
-        '''
+
+def main():
+    ttt=TicTacGame()
+    ttt.gameLoop()
+
+
+if __name__ == '__main__':
+    main()
+
+'''
     def testWinState(self)
 {
 for (var i=0;i < this.winstates.Length;i++){
