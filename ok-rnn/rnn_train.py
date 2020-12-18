25ceac6bcdb4eb92a554fad75df535959e79f362
John Alamina <john.alamina@gmail.com>
2020-12-18 06:40:12 +0000

    okc07

--- ok-rnn/rnn_train.py
+++ ok-rnn/rnn_train.py
@@ -136,7 +136,7 @@ sess.run(init)
 step = 0
 
 # training loop
-for x, y_, epoch in txt.rnn_minibatch_sequencer(codetext, BATCHSIZE, SEQLEN, nb_epochs=15):
+for x, y_, epoch in txt.rnn_minibatch_sequencer(codetext, BATCHSIZE, SEQLEN, nb_epochs=20):
 
     # train on one minibatch
     feed_dict = {X: x, Y_: y_, Hin: istate, lr: learning_rate, pkeep: dropout_pkeep, batchsize: BATCHSIZE}
